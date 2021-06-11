5160e95e877b252017a65d8a5ea40dedeaaac5ec
benamaurer <ben.a.maurer@gmail.com>
2021-06-11 18:29:15 -0500

    Adding capability to translate 835 information to descriptions.

--- interpreter.py
+++ interpreter.py
@@ -3,11 +3,13 @@ import pandas as pd
 from beeprint import pp
 from csv_info_to_dict import csv_dict_conversion
 
+
 pd.set_option('display.max_columns', None)
 pd.set_option('display.max_rows', None)
-pd.set_option('display.max_colwidth', None)
 
-df = pd.DataFrame(columns=['loop_ID', 'segment_ID', 'data_element_ID', 'data_element_value'])
+
+df_835_raw = pd.DataFrame(columns=['loop_ID', 'segment_ID', 'data_element_ID', 'data_element_value'])
+
 loop_ids = ['none','1000A','1000B','2000','2100','2110','none']
 loop_initiators = ['N1','LX','CLP','SVC','PLB']
 # loop_ids = {'1000A':'N1','1000B':'N1','2000':'LX','2100':'CLP','2110':'SVC','none':'PLB'}
@@ -20,18 +22,19 @@ dict_segments = csv_dict_conversion('segments.csv')
 dict_segment_ids = csv_dict_conversion('segment_ids.csv')
 
 
-#import of 835 code table
-def era_table_import():
-    try:
-        with open(era_params_file_name, mode='r') as infile:
-            reader = csv.reader(infile)
-            with open(era_params_file_name, mode='w') as outfile:
-                writer = csv.writer(outfile)
-                era_codes = {rows[0]:rows[1] for rows in reader}
-
-        return era_codes
-    except Exception as e:
-        return f"Unable to access era parameters file. Error code: {e}"
+# DEPRECATED: replaced by the above dicts
+# #import of 835 code table
+# def era_table_import():
+#     try:
+#         with open(era_params_file_name, mode='r') as infile:
+#             reader = csv.reader(infile)
+#             with open(era_params_file_name, mode='w') as outfile:
+#                 writer = csv.writer(outfile)
+#                 era_codes = {rows[0]:rows[1] for rows in reader}
+#
+#         return era_codes
+#     except Exception as e:
+#         return f"Unable to access era parameters file. Error code: {e}"
 
 
 def era_file_to_str(era_file_name):
@@ -43,7 +46,7 @@ def era_file_to_str(era_file_name):
         return 'Unable to find/convert era file.'
 
 
-def era_translate(era_codes, era_string):
+def era_decode(era_codes, era_string):
 
     segments = era_string.split('~')
 
@@ -67,16 +70,28 @@ def era_translate(era_codes, era_string):
             if segment_value == '':
                 continue
             if len(str(segment_value_id)) < 2:
-                df.loc[len(df.index)] = [str(loop_id), str(segment_name),'0' + str(segment_value_id),str(segment_value)]
+                df_835_raw.loc[len(df_835_raw.index)] = [str(loop_id), str(segment_name),'0' + str(segment_value_id),str(segment_value)]
             else:
-                df.loc[len(df.index)] = [str(loop_id), str(segment_name),str(segment_value_id), str(segment_value)]
+                df_835_raw.loc[len(df_835_raw.index)] = [str(loop_id), str(segment_name),str(segment_value_id), str(segment_value)]
             segment_value_id = segment_value_id + 1
 
-    return df
+    return df_835_raw
+
+
+def era_translate(era_df,era_code_dicts=[]):
+    df_835_translated = pd.DataFrame(columns=['segment_category', 'segment_name','segment_value'])
+    for index, row in era_df.iterrows():
+        translated_segment = dict_segments.get(str(row['segment_ID']))
+        translated_segment_ID = dict_segment_ids.get(str(row['segment_ID']) + str(row['data_element_ID']))
+        df_835_translated.loc[len(df_835_translated.index)] = [translated_segment,translated_segment_ID,str(row['data_element_value'])]
+        print()
+    return df_835_translated
 
 
 if __name__ == '__main__':
-    print(era_translate('',era_file_to_str('835test.txt')))
+    raw_835 = era_decode('',era_file_to_str('835test.txt'))
+    print(raw_835)
+    print(era_translate(raw_835,era_code_dicts=[dict_segments,dict_segment_ids]))
     # pp(dict_loops)
     # pp(dict_segments)
     # pp(dict_segment_ids)
